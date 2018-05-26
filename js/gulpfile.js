3a78f90c2670a39fe079b2e8cf0eb5e43888000e
zjzhi <153680611@qq.com>
2018-05-26 10:01:14 +0800

    项目初始结构

---
+++ js/gulpfile.js
@@ -0,0 +1,34 @@
+//使用包
+var gulp = require("gulp");
+
+//scss转换成css
+var sass = require('gulp-sass');
+//页面刷新
+var browserSync = require('browser-sync');
+
+//定义任务 sass任务
+gulp.task("sass", function() {
+    //导入文件
+    gulp.src("../sass/test.scss")
+        //转代码
+        .pipe(sass().on('error', sass.logError))
+        //导出文件
+        .pipe(gulp.dest("../css/"))
+})
+//修改文件即刷新浏览器
+gulp.task('myserver', () => {
+    // 开启服务器
+    browserSync({
+        server: '../../src',
+        // 端口
+        port: 10087,
+        //需要监听的文件路径
+        files: ['../sass/*.scss']
+    });
+
+    // 监听sass文件修改
+    gulp.watch('../sass/*.scss', ['sass']);
+   
+});
+//默认处理的任务序列
+gulp.task('default', ['sass','myserver']);
