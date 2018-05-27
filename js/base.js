fe5be63a047728ffb453f591312a9bf47519a75f
zjzhi <153680611@qq.com>
2018-05-27 13:07:37 +0800

    添加了轮播图图“

--- js/base.js
+++ js/base.js
@@ -1,13 +1,16 @@
 ;requirejs(['jquery-3.3.1','base'],function(){
-    $('.nav_list').find('li').has('ul').addClass('has-menu').append('<i class="fa fa-chevron-right"></i>');
-            $('.nav_list').on('mouseover','.has-menu',function(){
-                clearInterval(this.timer);
-                $(this).children('ul').show(0);
-            })
-            .on('mouseout','.has-menu',function(){
-                this.timer = setTimeout(()=>{
-                    $(this).children('ul').hide();
-                }, 0);
-                
-            });
+    $('.li1').addClass('has-menu').append('<i class="fa fa-chevron-right"></i>');
+    $('.li2').append('<i class="fa fa-chevron-right"></i>');
+    $('.nav_list').on('mouseover','.has-menu',function(){
+        $(this).children('i').hide();
+        $(this).children('div').show();
+        // $(this).removeChild('.fa');
+    })
+    .on('mouseout','.has-menu',function(){
+        $(this).children('i').show();
+        $(this).children('div').hide();
+        // $(this).append('<i class="fa fa-chevron-right"></i>')
+    });
+    $('.nav_right').append("<img src='../img/nav_right.png'/>");
+
 });
